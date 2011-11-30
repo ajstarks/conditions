11351dd876547085c30d0e0e7d81061dd8bcdf81
Stephen Ramsay <sramsay.unl@gmail.com>
2011-11-29 20:58:16 -0600

    Patch from ajstarks.  Commented functions in utils.go, and removed go-pkg-optargs dependency.

--- src/utils.go
+++ src/utils.go
@@ -27,127 +27,108 @@
 package utils
 
 import (
-  "fmt"
-  "io/ioutil"
-  "os"
-  "regexp"
-  "strings"
-  "json"
-  "github.com/jteeuwen/go-pkg-optarg"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"regexp"
+	"strings"
+	"json"
+	"flag"
 )
 
+// GetVersion returns the version of the package
 func GetVersion() string {
-
-  const VERS = "2.0.0"
-
-  return VERS
-
+	const VERS = "2.0.0"
+	return VERS
 }
 
-func GetConf() (string,string) {
+// GetConf returns the API key and weather station from
+// the configuration file at $HOME/.condrc
+func GetConf() (string, string) {
 
-  type config struct {
+	type config struct {
+		Key     string
+		Station string
+	}
 
-    Key  string
-    Station string
+	var b []byte
+	var conf config
 
-  }
+	var confFile = os.Getenv("HOME") + "/.condrc"
+	b, err := ioutil.ReadFile(confFile)
 
-  var b []byte
-  var conf config
-
-
-    var confFile = os.Getenv("HOME") + "/.condrc"
-    b, err := ioutil.ReadFile(confFile)
-
-    if err == nil {
-      jsonErr := json.Unmarshal(b,  &conf)
-      CheckError(jsonErr)
-    } else {
-      fmt.Println("You must create a .condrc file in $HOME.")
-      os.Exit(1)
-    }
-
-  return conf.Key, conf.Station
-
+	if err == nil {
+		jsonErr := json.Unmarshal(b, &conf)
+		CheckError(jsonErr)
+	} else {
+		fmt.Println("You must create a .condrc file in $HOME.")
+		os.Exit(1)
+	}
+	return conf.Key, conf.Station
 }
 
+// Options handles commandline options and returns a 
+// possibly updated weather station string
 func Options() string {
 
-  optarg.Add("s", "station", "Weather station.  May be indicated using \"city, state-abbreviation\", (US or Canadian) zipcode, 3- or 4-letter airport code, or LAT,LONG", "KLNK")
-  optarg.Add("h", "help", "Print this message", false)
-  optarg.Add("V", "version", "Print version number", false)
+	var help, version bool
+	var station, sconf string
 
-  _,station := GetConf()
+	_, sconf = GetConf()
+	if sconf == "" {
+		sconf = "KLNK"
+	}
 
-  if station == "" {
-    station = "KLNK"
-  }
+	flag.BoolVar(&help, "h", false, "print this message")
+	flag.BoolVar(&version, "v", false, "Print version number")
+	flag.StringVar(&station, "s", sconf, "Weather station: \"city, state-abbreviation\", (US or Canadian) zipcode, 3- or 4-letter airport code, or LAT,LONG")
+	flag.Parse()
 
-  var help, version bool
+	if help {
+		flag.PrintDefaults()
+		os.Exit(0)
+	}
 
-  for opt := range optarg.Parse() {
-    switch opt.ShortName {
-    case "s":
-      station = opt.String()
-    case "h":
-      help = opt.Bool()
-    case "V":
-      version = opt.Bool()
-    }
-  }
+	if version {
+		fmt.Println("conditions " + GetVersion())
+		fmt.Println("Copyright (C) 2011 by Stephen Ramsay")
+		fmt.Println("Data courtesy of Weather Underground, Inc.")
+		fmt.Println("is subject to Weather Underground Data Feed")
+		fmt.Println("Terms of Service.  The program itself is free")
+		fmt.Println("software, and you are welcome to redistribute")
+		fmt.Println("it under certain conditions.  See LICENSE for")
+		fmt.Println("details.")
+		os.Exit(0)
+	}
 
-  if help {
-    optarg.Usage()
-    os.Exit(0)
-  }
+	// Trap for city-state combinations (e.g. "San Francisco, CA") and
+	// make them URL-friendly (e.g. "CA/SanFranciso")
 
-  if version {
-    fmt.Println("conditions " + GetVersion())
-    fmt.Println("Copyright (C) 2011 by Stephen Ramsay")
-    fmt.Println("Data courtesy of Weather Underground, Inc.")
-    fmt.Println("is subject to Weather Underground Data Feed")
-    fmt.Println("Terms of Service.  The program itself is free")
-    fmt.Println("software, and you are welcome to redistribute")
-    fmt.Println("it under certain conditions.  See LICENSE for")
-    fmt.Println("details.")
-    os.Exit(0)
-  }
+	cityStatePattern := regexp.MustCompile("([A-Za-z ]+), ([A-Za-z ]+)")
+	cityState := cityStatePattern.FindStringSubmatch(station)
 
-  // Trap for city-state combinations (e.g. "San Francisco, CA") and
-  // make them URL-friendly (e.g. "CA/SanFranciso")
-
-  cityStatePattern := regexp.MustCompile("([A-Za-z ]+), ([A-Za-z ]+)")
-  cityState := cityStatePattern.FindStringSubmatch(station)
-
-  if cityState != nil {
-    station = cityState[2] + "/" + cityState[1]
-    station = strings.Replace(station, " ", "_", -1)
-  }
-
-  return station
-
+	if cityState != nil {
+		station = cityState[2] + "/" + cityState[1]
+		station = strings.Replace(station, " ", "_", -1)
+	}
+	return station
 }
 
-
+// BuildURL returns the URL required by the Weather Underground API
+// from the query type, station id, and API key
 func BuildURL(infoType string, stationId string, key string) string {
 
-  const URLstem = "http://api.wunderground.com/api/"
-  const query   = "/q/"
-  const format  = ".json"
+	const URLstem = "http://api.wunderground.com/api/"
+	const query = "/q/"
+	const format = ".json"
 
-  var URL string
-
-  URL = URLstem + key + "/" + infoType + query + stationId + format
-
-  return URL
-
+	return URLstem + key + "/" + infoType + query + stationId + format
 }
 
-
+// CheckError exits on error with a message
 func CheckError(err os.Error) {
-  if err != nil {
-    fmt.Println(os.Stderr, "Fatal error ", err.String())
-    os.Exit(1)
-  }
+	if err != nil {
+		fmt.Println(os.Stderr, "Fatal error ", err.String())
+		os.Exit(1)
+	}
 }
