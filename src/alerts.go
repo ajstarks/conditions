fe9d27f898a48bd5ecc6a07213a63af52df8805f
Anthony Starks <ajstarks@gmail.com>
2011-11-30 23:14:04 -0500

    Update src/alerts.go

--- src/alerts.go
+++ src/alerts.go
@@ -14,12 +14,12 @@
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation; either version 3, or (at your option) any
   later version.
-  
+
   alerts is distributed in the hope that it will be useful, but
   WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
   or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
   for more details.
-  
+
   You should have received a copy of the GNU General Public License
   along with alerts; see the file COPYING.  If not see
   <http://www.gnu.org/licenses/>
@@ -29,59 +29,41 @@ package main
 
 import (
   "./utils"
-  "fmt"
-  "http"
-  "json"
-  "io/ioutil"
+	"fmt"
+	"json"
 )
 
 type Conditions struct {
-  Alerts []Alerts
+	Alerts []Alerts
 }
 
 type Alerts struct {
-  Date        string
-  Expires     string
-  Description string
-  Message     string
+	Date        string
+	Expires     string
+	Description string
+	Message     string
 }
 
 func main() {
-
-  key,_ := utils.GetConf()
-
-  var stationId = utils.Options()
-  var URL string
-
-  URL = utils.BuildURL("alerts", stationId, key)
-
-  res, err := http.Get(URL)
-  var b []byte
-  var obs Conditions
-
-  if err == nil {
-    b, err = ioutil.ReadAll(res.Body)
-    res.Body.Close()
-    jsonErr := json.Unmarshal(b, &obs)
-    utils.CheckError(jsonErr)
-    printWeather(&obs, stationId)
-  }
+	var obs Conditions
+	key, _ := utils.GetConf()
+	stationId := utils.Options()
+	url := utils.BuildURL("alerts", stationId, key)
+	b, err := utils.Fetch(url)
+	utils.CheckError(err)
+	jsonErr := json.Unmarshal(b, &obs)
+	utils.CheckError(jsonErr)
+	printWeather(&obs, stationId)
 }
 
 func printWeather(obs *Conditions, stationId string) {
-
-  var alertsNum = len(obs.Alerts)
-  var alerts    = obs.Alerts
-
-  if alertsNum == 0 {
-    fmt.Println("No active alerts")
-  } else {
-    for i := 0; i < alertsNum; i++ {
-      fmt.Println("### " +  alerts[i].Description + " ###")
-      fmt.Println()
-      fmt.Println("Issued at " + alerts[i].Date)
-      fmt.Println("Expires at " + alerts[i].Expires)
-      fmt.Println(alerts[i].Message)
-    }
-  }
+	if len(obs.Alerts) == 0 {
+		fmt.Println("No active alerts")
+	} else {
+		fmt.Printf("Station: %s\n", stationId)
+		for _, a := range obs.Alerts {
+			fmt.Printf("### %s ###\n\nIssued at %s\nExpires at %s\n%s\n",
+				a.Description, a.Date, a.Expires, a.Message)
+		}
+	}
 }
