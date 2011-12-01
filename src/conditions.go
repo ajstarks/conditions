6895f9596cd4642bb8d01eebaa7540a5b44da034
Anthony Starks <ajstarks@gmail.com>
2011-11-30 23:19:25 -0500

    Update src/conditions.go

--- src/conditions.go
+++ src/conditions.go
@@ -31,89 +31,76 @@ package main
 
 import (
   "./utils"
-  "fmt"
-  "http"
-  "json"
-  "io/ioutil"
-  "regexp"
+	"fmt"
+	"json"
+	"regexp"
 )
 
 type Conditions struct {
-  Current_observation Current
+	Current_observation Current
 }
 
 type Current struct {
-  Observation_time     string
-  Observation_location Location
-  Station_id           string
-  Weather              string
-  Temperature_string   string
-  Relative_humidity    string
-  Wind_string          string
-  Pressure_mb          string
-  Pressure_in          string
-  Pressure_trend       string
-  Dewpoint_string      string
-  Heat_index_string    string
-  Windchill_string     string
-  Visibility_mi        string
-  Precip_today_string  string
+	Observation_time     string
+	Observation_location Location
+	Station_id           string
+	Weather              string
+	Temperature_string   string
+	Relative_humidity    string
+	Wind_string          string
+	Pressure_mb          string
+	Pressure_in          string
+	Pressure_trend       string
+	Dewpoint_string      string
+	Heat_index_string    string
+	Windchill_string     string
+	Visibility_mi        string
+	Precip_today_string  string
 }
 
 type Location struct {
-  Full  string
+	Full string
 }
 
 func main() {
-
-  key,_ := utils.GetConf()
-
-  var stationId = utils.Options()
-  var URL string
-
-  URL = utils.BuildURL("conditions", stationId, key)
-
-  res, err := http.Get(URL)
-  var b []byte
-  var obs Conditions
-
-  if err == nil {
-    b, err = ioutil.ReadAll(res.Body)
-    res.Body.Close()
-    jsonErr := json.Unmarshal(b, &obs)
-    utils.CheckError(jsonErr)
-    printWeather(&obs)
-  }
+	var obs Conditions
+	key, _ := utils.GetConf()
+	stationId := utils.Options()
+	url := utils.BuildURL("conditions", stationId, key)
+	b, err := utils.Fetch(url)
+	utils.CheckError(err)
+	jsonErr := json.Unmarshal(b, &obs)
+	utils.CheckError(jsonErr)
+	printWeather(&obs)
 }
 
 func printWeather(obs *Conditions) {
-
-  var current = obs.Current_observation
-
-  fmt.Println("Current conditions at " + current.Observation_location.Full + " (" + current.Station_id + ")")
-  fmt.Println(current.Observation_time)
-  fmt.Println("   Temperature: " + current.Temperature_string)
-  fmt.Println("   Sky Conditions: " + current.Weather)
-  fmt.Println("   Wind: " + current.Wind_string)
-  fmt.Println("   Relative humidity: " + current.Relative_humidity)
-  switch current.Pressure_trend {
-  case "+":
-      fmt.Println("   Pressure: " + current.Pressure_in + " in (" + current.Pressure_mb + " mb) and rising")
-  case "-":
-    fmt.Println("   Pressure: " + current.Pressure_in + " in (" + current.Pressure_mb + " mb) and falling")
-  case "0":
-    fmt.Println("   Pressure: " + current.Pressure_in + " in (" + current.Pressure_mb + " mb) and holding steady")
-  }
-  fmt.Println("   Dewpoint: " + current.Dewpoint_string)
-  if current.Heat_index_string != "NA" {
-    fmt.Println("   Heat Index: " + current.Heat_index_string)
-  }
-  if current.Windchill_string != "NA" {
-    fmt.Println("   Windchill: " + current.Windchill_string)
-  }
-  fmt.Println("   Visibility: " + current.Visibility_mi + " miles")
-  m,_ := regexp.MatchString("0.0", current.Precip_today_string)
-  if !m {
-    fmt.Println("   Precipitation today: " + current.Precip_today_string)
-  }
+	current := obs.Current_observation
+	fmt.Printf("Current conditions at %s (%s)\n%s\n",
+		current.Observation_location.Full, current.Station_id, current.Observation_time)
+	fmt.Println("   Temperature:", current.Temperature_string)
+	fmt.Println("   Sky Conditions:", current.Weather)
+	fmt.Println("   Wind:", current.Wind_string)
+	fmt.Println("   Relative humidity:", current.Relative_humidity)
+	pstring := fmt.Sprintf("   Pressure: %s in (%s mb) and", current.Pressure_in, current.Pressure_mb)
+	switch current.Pressure_trend {
+	case "+":
+		fmt.Println(pstring, "rising")
+	case "-":
+		fmt.Println(pstring, "falling")
+	case "0":
+		fmt.Println(pstring, "holding steady")
+	}
+	fmt.Println("   Dewpoint: " + current.Dewpoint_string)
+	if current.Heat_index_string != "NA" {
+		fmt.Println("   Heat Index: " + current.Heat_index_string)
+	}
+	if current.Windchill_string != "NA" {
+		fmt.Println("   Windchill: " + current.Windchill_string)
+	}
+	fmt.Printf("   Visibility: %s miles\n", current.Visibility_mi)
+	m, _ := regexp.MatchString("0.0", current.Precip_today_string)
+	if !m {
+		fmt.Println("   Precipitation today:", current.Precip_today_string)
+	}
 }
