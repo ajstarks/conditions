e34a82be2e51e403a6574133f9dbf5105d95699e
Anthony Starks <ajstarks@gmail.com>
2011-11-30 23:12:05 -0500

    Update src/astronomy.go

--- src/astronomy.go
+++ src/astronomy.go
@@ -12,12 +12,12 @@
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation; either version 3, or (at your option) any
   later version.
-  
+
   alerts is distributed in the hope that it will be useful, but
   WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
   or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
   for more details.
-  
+
   You should have received a copy of the GNU General Public License
   along with alerts; see the file COPYING.  If not see
   <http://www.gnu.org/licenses/>
@@ -27,83 +27,75 @@ package main
 
 import (
   "./utils"
-  "fmt"
-  "http"
-  "json"
-  "io/ioutil"
-  "strconv"
+	"fmt"
+	"json"
+	"strconv"
 )
 
-
 type Conditions struct {
-  Moon_phase Moon_phase
-  Sunrise    Sunrise
-  Sunset     Sunset
+	Moon_phase Moon_phase
+	Sunrise    Sunrise
+	Sunset     Sunset
 }
 
 type Moon_phase struct {
-  PercentIlluminated  string
-  AgeOfMoon           string
-  Sunrise             Sunrise
-  Sunset              Sunset
+	PercentIlluminated string
+	AgeOfMoon          string
+	Sunrise            Sunrise
+	Sunset             Sunset
 }
 
 type Sunrise struct {
-  Hour    string
-  Minute  string
+	Hour   string
+	Minute string
 }
 
 type Sunset struct {
-  Hour    string
-  Minute  string
+	Hour   string
+	Minute string
 }
-
 
 func main() {
-
-  key,_ := utils.GetConf()
-
-  var stationId = utils.Options()
-  var URL string
-
-  URL = utils.BuildURL("astronomy", stationId, key)
-
-  res, err := http.Get(URL)
-  var b []byte
-  var obs Conditions
-
-  if err == nil {
-    b, err = ioutil.ReadAll(res.Body)
-    res.Body.Close()
-    jsonErr := json.Unmarshal(b, &obs)
-    utils.CheckError(jsonErr)
-    printWeather(&obs, stationId)
-  }
+	var obs Conditions
+	key, _ := utils.GetConf()
+	stationId := utils.Options()
+	url := utils.BuildURL("astronomy", stationId, key)
+	b, err := utils.Fetch(url)
+	utils.CheckError(err)
+	jsonErr := json.Unmarshal(b, &obs)
+	utils.CheckError(jsonErr)
+	printWeather(&obs, stationId)
 }
-
 
 func printWeather(obs *Conditions, stationId string) {
 
-  var age,_ = strconv.Atoi(obs.Moon_phase.AgeOfMoon)
-  var percent = obs.Moon_phase.PercentIlluminated
+	var age, _ = strconv.Atoi(obs.Moon_phase.AgeOfMoon)
+	var moonDesc string
 
-  // Calculate traditional description of lunar phase
+	// Calculate traditional description of lunar phase
 
-  var moonDesc string
-
-  switch {
-  case age < 2:  moonDesc = "New moon"
-  case age < 6:  moonDesc = "Waxing crescent"
-  case age < 9:  moonDesc = "First quarter"
-  case age < 13: moonDesc = "Waxing gibbous"
-  case age < 17: moonDesc = "Full moon"
-  case age < 20: moonDesc = "Waning gibbous"
-  case age < 24: moonDesc = "Last quarter"
-  case age < 28: moonDesc = "Waning crescent"
-  }
-
-  fmt.Println("Moon Phase: " + moonDesc + " (" + percent + "% illuminated)")
-  fmt.Println("Sunrise: " + obs.Moon_phase.Sunrise.Hour + ":" + obs.Moon_phase.Sunrise.Minute)
-  fmt.Println("Sunset: " + obs.Moon_phase.Sunset.Hour + ":" + obs.Moon_phase.Sunset.Minute)
-
+	switch {
+	case age < 2:
+		moonDesc = "New moon"
+	case age < 6:
+		moonDesc = "Waxing crescent"
+	case age < 9:
+		moonDesc = "First quarter"
+	case age < 13:
+		moonDesc = "Waxing gibbous"
+	case age < 17:
+		moonDesc = "Full moon"
+	case age < 20:
+		moonDesc = "Waning gibbous"
+	case age < 24:
+		moonDesc = "Last quarter"
+	case age < 28:
+		moonDesc = "Waning crescent"
+	}
+	sr := obs.Moon_phase.Sunrise
+	ss := obs.Moon_phase.Sunset
+	percent := obs.Moon_phase.PercentIlluminated
+	fmt.Printf("Moon Phase: %s (%s%% illuminated)\n", moonDesc, percent)
+	fmt.Printf("Sunrise   : %s:%s\n", sr.Hour, sr.Minute)
+	fmt.Printf("Sunset    : %s:%s\n", ss.Hour, ss.Minute)
 }
